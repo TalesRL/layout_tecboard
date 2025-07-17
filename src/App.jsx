5b833bf4c0f60ccdb3009cca4193d78001884848
TalesRL <talesleite.r@gmail.com>
2025-07-17 15:06:35 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import { Banner } from './componentes/Banner'
 import { Card } from './componentes/evento'
@@ -6,7 +7,6 @@ import { Tema } from './componentes/Tema'
 
 
 function App() {
-
 
   const temas = [
     {
@@ -34,30 +34,51 @@ function App() {
       nome: 'cloud'
     }
   ]
-  const eventos = [
+  const [eventos, setEventos] = useState([
     {
-      capa:'https://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png',
+      capa: 'https://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png',
       tema: temas[0],
-      data: new Date (),
+      data: new Date(),
       titulo: 'Mulheres no Front'
     }
-  ]
-
+  ])
+  function adicionarEvento(evento) {
+    //eventos.push(evento) ====acho que não usa com o useState
+    //console.log('eventos =>', eventos)
+    setEventos([...eventos, evento])
+  }
   return (
     <main>
       <header >
         <img src="/logo.png" alt="logo" />
       </header>
       <Banner />
-      <Formulario />
-      {temas.map(function (item) {
-        return (
-          <section key={item.id}>
-            <Tema tema={item} />
-            <Card evento={eventos[0]}/>
-          </section>
-        )
-      })}
+      <Formulario temas={temas}
+        aoSubmeter={adicionarEvento}
+      />
+      <section className="container">
+        {temas.map(function (tema) {
+          if (!eventos.some(function (evento) {
+            return evento.tema.id == tema.id
+          })){
+            return null
+          }
+          return (
+            <section key={tema.id}>
+              <Tema tema={tema} />
+              <div className="eventos">
+                {eventos.filter(function (evento) {
+                  return evento.tema.id == tema.id
+                })
+                  .map(function (evento, indice) {
+                    return <Card evento={evento} key={indice} />
+                  })}
+              </div>
+            </section>
+          )
+        })}
+      </section>
+
     </main>
   )
 }
--- src/componentes/Fomulario/index.jsx
+++ src/componentes/Fomulario/index.jsx
@@ -7,10 +7,23 @@ import { ListaSuspensa } from "../ListaSuspensa";
 import { TituloFormulario } from "../TituloFormulario";
 import './formulario.estilo.css'
 
-export function Formulario() {
+export function Formulario({temas, aoSubmerter}) {
+
+  function aoFormSubmetido (formData) {
+    console.log('novo evento', formData)
+    const evento = {
+      capa: formData.get('capa'),
+      tema: temas.find(function (item) {
+        return item.id == formData.get ('tema')
+      }),
+      data: new Date(formData.get('dataEvento')),
+      titulo: formData.get('nome')
+    }
+    aoSubmeter(evento)
+  }
 
   return (
-    <form className='form'>
+    <form className='form' action={aoFormSubmetido}>
       <TituloFormulario>
         Preencha para criar um evento:
       </TituloFormulario>
@@ -21,17 +34,25 @@ export function Formulario() {
           </Label>
           <CampoDeEntrada type="text" id='nome' placeholder='Summer Dev' />
         </CampoDoFormulario>
+         <CampoDoFormulario>
+          <Label htmlFor="capa">
+            Qual o endereço da imagem de capa?
+          </Label>
+          <CampoDeEntrada type="text" id='capa' placeholder='http://...' />
+        </CampoDoFormulario>
         <CampoDoFormulario>
           <Label htmlFor="dataEvento">
             Data do evento
           </Label>
-          <CampoDeEntrada type="date" id='dataEvento' placeholder='XX/XX/XXXX' />
+          <CampoDeEntrada type="date" 
+          id='dataEvento' 
+          name='XX/XX/XXXX' />
         </CampoDoFormulario>
         <CampoDoFormulario>
-          <Label htmlFor="dataEvento">
+          <Label htmlFor="tema">
             Tema do evento
           </Label>
-          <ListaSuspensa />
+          <ListaSuspensa id="tema" name="tema" itens={temas}/>
         </CampoDoFormulario>
       </div>
       <div className="acoes">
